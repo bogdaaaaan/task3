4ca1f5f1a7da35a8e70ae21b150b9ab7cce4d074
bogdaaaaan <bogdan.glechkovskiy@gmail.com>
2020-10-20 23:28:37 +0300

    made 3 blocks

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    let x = document.querySelector("#my-nav-menu");
+    document.querySelector("#menu").onclick = function(){
+         x.classList.toggle('responsive');
+    }
+    
+}
